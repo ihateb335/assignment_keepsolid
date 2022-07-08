260f44c920ca1fcacd461547638a9f7c814c84bf
MrZahn <mykhailo.zhar@stud.onu.edu.ua>
2022-07-08 12:31:43 +0300

    Initial

---
+++ database/migrations/2022_07_08_090253_create_authors_books.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors_books', function (Blueprint $table) {
+            $table->id();
+            
+            $table
+            ->foreignId('author_id')
+            ->constrained('authors')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+
+            $table 
+            ->foreignId('book_id')
+            ->constrained('books')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors_books');
+    }
+};
